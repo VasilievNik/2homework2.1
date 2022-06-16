9ecc80f4e3a1505bb2881bd19cf4b9d2970281f6
nikit <nikita.veselyev@gmail.com>
2022-06-17 00:02:52 +0300

    1st commit

---
+++ src/com/company/Bicycle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Bicycle {
+
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
